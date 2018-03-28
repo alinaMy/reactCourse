cd2917cb91e5524c36973b83f4a1b2f77680f35e
amyunster <alina.myunster@gmail.com>
2018-03-28 10:18:02 -0700

    added bind

--- src/App.js
+++ src/App.js
@@ -12,17 +12,28 @@ class App extends Component {
     ]
 }
 
-switchNameHandler = () =>{
-    console.log('was clicked')
+switchNameHandler = (newName) =>{
+    //console.log('was clicked')
+  this.setState({
+    persons: [
+      {name: newName, age: 36},
+      {name: 'Eugene Myunster', age: 37},
+      {name: 'Jonathan Myunster', age: 3}
+    ]
+  })
 }
   render() {
     return (
       <div className="App">
         <h1> Let's do React</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>My hobbie: comps</Person>
-        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
+        <button onClick={this.switchNameHandler.bind(this, 'Alina Myunster')}>Switch name</button>
+        <Person name = {this.state.persons[0].name}
+                age = {this.state.persons[0].age}/>
+        <Person name = {this.state.persons[1].name}
+                age = {this.state.persons[1].age}
+                click = {this.switchNameHandler.bind(this, 'Alina Kataeva')}>My hobbie: comps</Person>
+        <Person name = {this.state.persons[2].name}
+                age = {this.state.persons[2].age}/>
       </div>
     );
   }
